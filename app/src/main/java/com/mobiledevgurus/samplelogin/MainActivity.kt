acdc8ea9604870e9220a9a55b07add323ca278ae
Rommel Azucena <rcazucena@multisyscorp.com>
2022-06-28 23:57:56 +0800

    Initial commit

---
+++ app/src/main/java/com/mobiledevgurus/samplelogin/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.mobiledevgurus.samplelogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mobiledevgurus.samplelogin.ui.theme.SampleLoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleLoginTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreen() {
+
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var isPasswordVisible by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+
+            Spacer(modifier = Modifier.padding(top = 70.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo"
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "Email"
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = email,
+                onValueChange = { email = it },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
+
+            Spacer(modifier = Modifier.padding(top = 12.dp))
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "Password"
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = password,
+                onValueChange = { password = it },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    val image = if (isPasswordVisible) Icons.Filled.Visibility
+                    else Icons.Filled.VisibilityOff
+
+                    IconButton(
+                        modifier = Modifier.alpha(.3f),
+                        onClick = {
+                            isPasswordVisible = !isPasswordVisible
+                        }
+                    ) {
+                        Icon(
+                            imageVector = image,
+                            contentDescription = "Password visibility icon"
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.padding(top = 12.dp))
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Login")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/mobiledevgurus/samplelogin/ui/theme/Theme.kt
@@ -0,0 +1,45 @@
+package com.mobiledevgurus.samplelogin.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.Black,
+    primaryVariant = Color(0xFF030303),
+    secondary = Color(0xFF383838),
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color.Black,
+    primaryVariant = Color(0xFF030303),
+    secondary = Color(0xFF383838),
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun SampleLoginTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
